01109718ff8d4b5f519d599456bed5398a748beb
LeeWest89 <6683@holbertonstudents.com>
2024-08-07 17:20:28 -0500

    Forgot I made a modification to db.js, it is now reverted

--- back-end/login/db.js
+++ back-end/login/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',    // Database host
     user: 'root',         // Database user
-    password: '',     // Database password
+    password: 'root',     // Database password
     database: 'nostalgia_movie', // Database name
     waitForConnections: true,
     connectionLimit: 10,
