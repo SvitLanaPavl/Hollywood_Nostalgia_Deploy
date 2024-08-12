24cbf2f82b31225bfbe3526d3ea6991cfc2b7d25
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-08-11 19:09:41 -0500

    pagination is done, looks almost as expected yet not sure about the accuracy of its work as i do not have data to test

--- front-end/src/pages/Home/Home.jsx
+++ front-end/src/pages/Home/Home.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import Hero from '../../components/Hero/Hero'
 import Filters from '../../components/Filters/Filters'
 import MovieGrid from '../../components/MovieCards/MovieGrid'
+import Pagination from '../../components/Pagination/Pagination'
 
 const Home = () => {
+
+    // State for pagination
+    const [currentPage, setCurrentPage] = useState(1);
+    const moviesPerPage = 18; // 6 movies per row, 3 rows
+    const totalMovies = 620; // Not sure how many movies in the database
+  
+    const handlePageChange = (page) => {
+      setCurrentPage(page);
+      // We are supposed to fetch some data here... not sure yet
+    };
   return (
     <div className='home'>
       <Navbar />
       <Hero />
       <Filters />
       <MovieGrid />
+      <Pagination
+        currentPage={currentPage}
+        totalMovies={totalMovies}
+        moviesPerPage={moviesPerPage}
+        onPageChange={handlePageChange}
+      />
     </div>
   )
 }
