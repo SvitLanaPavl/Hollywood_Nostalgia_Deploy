2ad4956d07adb8dc3a4888542388d00a7f8facc1
Svitlana Pavlovska <lanapavlovska90@gmail.com>
2024-08-12 17:12:02 -0500

    the play page is done

--- front-end/src/components/MovieCards/MovieCards.jsx
+++ front-end/src/components/MovieCards/MovieCards.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import playIcon from '../../assets/play.svg';
 import heartIcon from '../../assets/heart.svg';
 import './MovieCards.css';
@@ -7,6 +8,7 @@ const MovieCard = ({ movie }) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isFavorite, setIsFavorite] = useState(false);
   const hoverTimeoutRef = useRef(null);
+  const navigate = useNavigate();
 
   const handleMouseEnter = () => {
     hoverTimeoutRef.current = setTimeout(() => {
@@ -23,6 +25,10 @@ const MovieCard = ({ movie }) => {
     setIsFavorite(!isFavorite);
   };
 
+  const handleWatchTrailer = () => {
+    navigate(`/trailer/${movie.id}`); // this will navigate to the player page with the movie ID from DB
+  }
+
   return (
     <div 
       className={`relative w-[201px] h-[294px] rounded-lg bg-white overflow-hidden transition-all duration-500 ease-in-out cursor-pointer ${
@@ -30,6 +36,7 @@ const MovieCard = ({ movie }) => {
       }`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
+      onClick={handleWatchTrailer}
     >
       <div
         className="w-full h-full bg-cover bg-center"
@@ -55,9 +62,14 @@ const MovieCard = ({ movie }) => {
         ) : (
           <div className="absolute bottom-0 w-full h-[75px] p-5 bg-[rgba(51,51,51,0.94)] flex items-center gap-2.5">
             <div className='flex justify-center items-center round hover:scale-125 transition ease-in-out duration-300'>
-              <img src={playIcon} alt="Play Icon" className="w-4 h-4" />
+              <img src={playIcon} alt="Play Icon"
+              className="w-4 h-4 cursor-pointer"
+              onClick={handleWatchTrailer} // This will navigate to trailer
+              />
             </div>
-            <span className="text-white font-kaisei text-lg ms-2 hover:text-primary hover:opacity-90 transition ease-in-out duration-300">Watch Trailer</span>
+            <span className="text-white font-kaisei text-lg ms-2 hover:text-primary hover:opacity-90 transition ease-in-out duration-300"
+            onClick={handleWatchTrailer}>
+              Watch Trailer</span>
           </div>
         )}
       </div>
