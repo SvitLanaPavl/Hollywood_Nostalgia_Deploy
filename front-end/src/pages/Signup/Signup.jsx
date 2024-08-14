7eadfb8afe9c33a1d473773588d237742ce2e237
meisibley <mkong99@yahoo.com>
2024-08-14 15:45:19 -0500

    Signup.jsx fix register endpoint

--- front-end/src/pages/Signup/Signup.jsx
+++ front-end/src/pages/Signup/Signup.jsx
@@ -14,7 +14,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault(); // Prevent the default form submission
     try {
-      const response = await fetch('http://localhost:3000/auth/signup', {
+      const response = await fetch('http://localhost:3000/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
